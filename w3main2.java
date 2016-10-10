dde7b9755cfe89753bb1b170f112803eb166da18
이상준 <hubls@naver.com>
2016-10-10 12:18:11 +0900

    Add files via upload

---
+++ w3main2.java
@@ -0,0 +1,13 @@
+﻿print ('home work') 
+class Person { 
+    private float weight; 
+    public void setWeight(float w) { 
+        this.weight=w; 
+    } 
+    private float getWeight() { 
+        return weight; 
+    } 
+} 
+Person h=new Person(); 
+h.setWeight((float)75.0); 
+System.out.println(h.getWeight()); 
