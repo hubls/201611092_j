67386636411d39d2a32818428e17c77ebc72f88c
이상준 <hubls@naver.com>
2016-10-10 12:18:22 +0900

    Add files via upload

---
+++ w4main.java
@@ -0,0 +1,32 @@
+﻿System.out.println("Homework"); 
+public class Rectangle { 
+    private int width; 
+    private int height; 
+    public Rectangle(int w, int h) { 
+        this.width=w; 
+        this.height=h; 
+    } 
+    public int getWidth() { 
+        return width; 
+    } 
+    public int getHeight() { 
+        return height; 
+    } 
+    public void setHeight(int h) { 
+        height=h; 
+    } 
+    public void setWidth(int newWidth){ 
+        width = newWidth; 
+    } 
+    public void setSize(int w, int h){ 
+        setWidth(w); 
+        setHeight(h); 
+    } 
+    public int calcArea() { 
+        return width*height; 
+    } 
+} 
+
+ 
+Rectangle h= new Rectangle(10,20) 
+System.out.println("Area is " + h.calcArea()); 
