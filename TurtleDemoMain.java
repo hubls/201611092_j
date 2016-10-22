31df88b15bd6d2a06243faefc88f455833d2a787
이상준 <hubls@naver.com>
2016-10-22 20:06:04 +0900

    거북이 사각형

---
+++ TurtleDemoMain.java
@@ -0,0 +1,14 @@
+﻿package com.sd.Turtle; 
+import ch.aplu.turtle.*; 
+
+class TurtleDemoMain { 
+	TurtleDemoMain(){ 
+		Turtle t1 = new Turtle(); 
+		for(int i=0; i<4; i++){ 
+			t1.forward(50); 
+			t1.left(90);} 
+	} 
+	public static void main(String[] args){ 
+		new TurtleDemoMain(); 
+	} 
+} 
