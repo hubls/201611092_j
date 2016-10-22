c69609385d0143e9a96a50cd7212ceb791eda007
이상준 <hubls@naver.com>
2016-10-22 20:06:48 +0900

    거북이 별

---
+++ Star.java
@@ -0,0 +1,14 @@
+﻿import ch.aplu.turtle.*; 
+
+ 
+Turtle t1 = new Turtle(); 
+class Star{ 
+	void drawStar(int size){ 
+		for(int i=0; i<5; i++){ 
+			t1.fd(size); 
+			t1.right(144); 
+		} 
+	} 
+} 
+Star a = new Star(); 
+a.drawStar(100); 
