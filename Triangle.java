d80e8c877139dd0798c031c68ee9382c2e68e1e2
이상준 <hubls@naver.com>
2016-10-22 20:06:30 +0900

    거북이 삼각형

---
+++ Triangle.java
@@ -0,0 +1,14 @@
+﻿import ch.aplu.turtle.*; 
+
+ 
+Turtle t1 = new Turtle(); 
+class Triangle{ 
+	void drawTriangle(int size){ 
+		for(int i=0; i<3; i++){ 
+			t1.fd(size); 
+			t1.right(120); 
+		} 
+	} 
+} 
+Triangle a = new Triangle(); 
+a.drawTriangle(100); 
