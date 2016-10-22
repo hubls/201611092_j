fe131e00a95262687a06f5a4ad5cacc35e2bfbc4
이상준 <hubls@naver.com>
2016-10-22 20:07:10 +0900

    거북이 사각형

---
+++ Square.java
@@ -0,0 +1,14 @@
+﻿import ch.aplu.turtle.*; 
+
+ 
+Turtle t1 = new Turtle(); 
+class Square{ 
+	void drawSquare(int size){ 
+		for(int i=0; i<4; i++){ 
+			t1.fd(size); 
+			t1.right(90); 
+		} 
+	} 
+} 
+Square a = new Square(); 
+a.drawSquare(100); 
