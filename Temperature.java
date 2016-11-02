b2a1a97f437ebb533a036557981d9b26c69e0537
이상준 <hubls@naver.com>
2016-11-02 22:17:59 +0900

    섭씨화씨 과제
    섭씨화씨 과제

---
+++ Temperature.java
@@ -0,0 +1,22 @@
+﻿class Temperature{
+	void change(String a, float b){
+			double c;
+			if(a.equals("F")){
+				c=(b-32)/1.8;
+				System.out.println(b+a+" is chaged to "+c+"C");
+				}
+			else if(a.equals("C")){
+				c = (b*1.8)+32;
+				System.out.println(b+a+" is chaged to "+c+"F");
+				}
+				else{
+					System.out.println("Error");
+				}
+			}
+			public static void main(String[] args){
+				Temperature c = new Temperature();
+				c.change("C", 20.0f);
+				Temperature f = new Temperature();
+				f.change("F", 20.0f);
+			}
+		}
