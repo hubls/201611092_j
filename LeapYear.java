5fab5d20966eccfcb70e990cc332d53b2069176d
이상준 <hubls@naver.com>
2016-11-06 23:47:48 +0900

    윤년
    윤년

---
+++ LeapYear.java
@@ -0,0 +1,19 @@
+﻿class LeapYear {
+	public void computeLeap(int[] y) {
+		for (int x: y) {
+			if (((x % 4 == 0) && (x % 100 != 0)) || (x % 400 == 0)) {
+				System.out.printf("%d is LeapYear\n", x);
+			}
+			else{
+				System.out.printf("%d isn't LeapYear\n", x);
+			}
+		}
+	}
+	
+	public static void main(String[] args) {
+		int[] year = { 1234, 1200, 1997, 1998, 2000, 1592, 2016 };
+		
+		LeapYear l = new LeapYear();
+		l.computeLeap(year);
+	}
+}
