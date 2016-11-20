a41d7a1d2b0d747a9e2f77187e4b6337eea7cc6e
이상준 <hubls@naver.com>
2016-11-20 16:06:44 +0900

    문자열에서 문자세기
    문자열에서 문자세기

---
+++ CountingChars.java
@@ -0,0 +1,25 @@
+﻿import java.util.*; 
+
+ 
+class CountingChars { 
+	 
+	static Map countChars(String str) { 
+		Map<String, Integer> wc = new HashMap<String, Integer>(); 
+		String t; 
+		char[] strarray = str.toCharArray(); 
+		 
+		for (char c : strarray) { 
+			t = Character.toString(c); 
+			if (wc.containsKey(t)) 
+				wc.put(t, wc.get(t) + 1); 
+			else 
+				wc.put(t, 1); 
+		} 
+		 
+		return wc; 
+	} 
+	 
+	public static void main(String[] args) { 
+		String letters = "sangmyung university"; 
+		System.out.println("'" + letters + "' counting result: " + countChars(letters)); 
+	} 
