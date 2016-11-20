2f337b0247056e309b1c2335c3e9cfe936be3ca7
이상준 <hubls@naver.com>
2016-11-20 16:08:03 +0900

    '학생' 객체를 정렬하기
    '학생' 객체를 정렬하기

---
+++ Student.java
@@ -0,0 +1,32 @@
+﻿import java.util.ArrayList; 
+import java.util.Collections; 
+
+ 
+class Student { 
+	private String name; 
+	private int stdNum; 
+	 
+	Student(String name, int snum) { 
+		this.name = name; 
+		this.stdNum = snum; 
+	} 
+	 
+	public String getName() { 
+		return name; 
+	} 
+	 
+	public static void main(String[] args) { 
+		ArrayList<String> snameList = new ArrayList<String>(); 
+		 
+		Student s1 = new Student("LDJ", 201611091); 
+		Student s2 = new Student("LSJ", 201611092); 
+		 
+		snameList.add(s2.getName()); 
+		snameList.add(s1.getName()); 
+		 
+		Collections.sort(snameList); 
+		 
+		for (String s : snameList) 
+			System.out.println(s); 
+	} 
+} 
