a6fc807344df8f7cdbcdbb27be3ab4427c9e7cbc
이상준 <hubls@naver.com>
2016-11-20 16:07:34 +0900

    hello뒤집기
    hello뒤집기

---
+++ Olleh.java
@@ -0,0 +1,17 @@
+﻿class Olleh { 
+	 
+	static void reverse(StringBuilder sb, String str) { 
+		char[] charh = str.toCharArray(); 
+		 
+		for (int i = charh.length - 1; i >= 0; i--) 
+			sb.append(charh[i]); 
+	} 
+	 
+	public static void main(String[] args) { 
+		String hello = "hello"; 
+		StringBuilder olleh = new StringBuilder(); 
+		 
+		reverse(olleh, hello); 
+		System.out.println("reverse " + hello + " then " + olleh.toString()); 
+	} 
+} 
