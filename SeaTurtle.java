ce48147dedf7105e80ed8ea6fb30ada0a2591e88
이상준 <hubls@naver.com>
2016-12-03 23:43:18 +0900

    Add files via upload

---
+++ SeaTurtle.java
@@ -0,0 +1,15 @@
+﻿package com.sd.ch8; 
+import com.sd.ch8.*; 
+ 
+public class SeaTurtle extends Turtle { 
+	public void swim(){ 
+		System.out.println("SeaTurtle swims"); 
+	} 
+	public static void main(String[] args){ 
+		SeaTurtle s = new SeaTurtle(); 
+		s.setFood("BaDa"); 
+		s.eat(); 
+		s.swim(); 
+		System.out.println("Land Turtle has "+s.nLimbs+" limbs"); 
+	} 
+}
