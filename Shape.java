d1425e958c3d5225271d1e845079308b94283720
이상준 <hubls@naver.com>
2016-12-03 23:57:46 +0900

    Add files via upload

---
+++ Shape.java
@@ -0,0 +1,13 @@
+﻿package com.sd.ch8; 
+import com.sd.ch8.*; 
+public abstract class Shape { 
+	public String name = "Shape"; 
+	public abstract double calcArea(); 
+	public abstract double calcPerimeter(); 
+	public String getname(){ 
+		return this.name; 
+	} 
+	public String toString(){ 
+		return "Shape..."+super.toString(); 
+	} 
+} 
