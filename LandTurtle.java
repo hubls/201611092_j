388a7bafbe696f1b5f8536f96dded8f155982984
이상준 <hubls@naver.com>
2016-12-03 23:42:42 +0900

    Add files via upload

---
+++ LandTurtle.java
@@ -0,0 +1,15 @@
+﻿package com.sd.ch8; 
+import com.sd.ch8.*; 
+ 
+public class LandTurtle extends Turtle { 
+	public void walk(){ 
+		System.out.println("LandTurtle walks"); 
+	} 
+	public static void main(String[] args){ 
+		LandTurtle l = new LandTurtle(); 
+		l.setFood("San"); 
+		l.eat(); 
+		l.walk(); 
+		System.out.println("Land Turtle has "+l.nLimbs+" limbs"); 
+	} 
+} 
