ffe49ea79ee26993890ee69aea8a861283be6a46
이상준 <hubls@naver.com>
2016-12-03 23:54:41 +0900

    Add files via upload

---
+++ Circle2.java
@@ -0,0 +1,28 @@
+﻿package com.sd.ch8; 
+import com.sd.ch8; 
+public class Circle extends Shape { 
+    private final double radius, area, perimeter; 
+
+ 
+    public Circle(double radius) { 
+        this.radius = radius; 
+        this.area = Math.PI * radius * radius; 
+        this.perimeter = 2 * Math.PI * radius; 
+    } 
+    @Override 
+    public double calcArea() { 
+        return this.area; 
+    } 
+    @Override 
+    public double calcPerimeter() { 
+        return this.perimeter; 
+    } 
+    public String toString() { 
+        return "Circle..."+super.toString(); 
+    } 
+    public static void main(String[] args){ 
+        Circle c = new Circle(5); 
+        System.out.println("Area is "+c.calcArea()); 
+        System.out.println("Perimeter is "+c.calcPerimeter()); 
+    } 
+} 
